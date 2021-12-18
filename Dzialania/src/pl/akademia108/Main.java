98d304acc2c6dfcd174e8ff1a5644ee0e85e9b14
Krystian Wieczorek <96172602+KrystianKrychu@users.noreply.github.com>
2021-12-18 14:37:02 +0100

    Add files via upload

---
+++ Dzialania/src/pl/akademia108/Main.java
@@ -0,0 +1,12 @@
+package pl.akademia108;
+
+import pl.akademia108.java.VariablesDeclarationInitialization;
+
+public class Main {
+
+    public static void main(String[] args) {
+        VariablesDeclarationInitialization variablesExample = new VariablesDeclarationInitialization();
+
+        variablesExample.variablesDeclarationInitializationExample();
+    }
+}
---
+++ Dzialania/src/pl/akademia108/java/VariablesDeclarationInitialization.java
@@ -0,0 +1,38 @@
+package pl.akademia108.java;
+
+public class VariablesDeclarationInitialization {
+    public void variablesDeclarationInitializationExample() {
+
+        /* Deklaracja zmiennej */
+        int age;
+
+        /* Inicjalizacja zmiennej */
+        age = 21;
+
+        System.out.println(age);
+
+        /* Przypisywanie nowej wartości do zmiennej */
+        int age2;
+
+        age2 = age + 9;
+
+        System.out.println(age2);
+
+        /* Deklaracja z inicjalizacją */
+        double price = 12.99;
+
+        System.out.println(price);
+
+        /* Przypisywanie nowej wartości do zmiennej */
+        double price2;
+        price2 = price + 3;
+
+        System.out.println(price2);
+
+
+
+
+
+    }
+
+}
